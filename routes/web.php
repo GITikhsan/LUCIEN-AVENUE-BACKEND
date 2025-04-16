9630b2b123f9471577efe271078b16663ca91bab
zaidanelha <zaidanelhaa@gmail.com>
2025-04-16 15:33:04 +0700

    test commit image

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('aboutus');
+    return view('welcome');
 });
 Route::get('/aboutus', function () {
     return view('aboutus');
