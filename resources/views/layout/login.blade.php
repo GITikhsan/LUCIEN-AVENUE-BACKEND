20f0b34237b666ade3706e6d3265682125f4ed88
iantasena <puantorian12@gmail.com>
2025-04-23 10:14:18 +0700

    mini change

--- resources/views/layout/login.blade.php
+++ resources/views/layout/login.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<<<<<<< Updated upstream
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Login</title>
@@ -56,5 +57,14 @@
       </div>
     </div>
   </div>
+=======
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    
+>>>>>>> Stashed changes
 </body>
 </html>
--- resources/views/layout/profile.blade.php
+++ resources/views/layout/profile.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Profile Page</title>
+  <title>Profile</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="m-0 font-sans bg-white">
