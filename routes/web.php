3860ee043813f94e410eba7d2fcbf3c73f31dbe2
iantasena <puantorian12@gmail.com>
2025-05-07 12:50:28 +0700

    rename employee

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/admin', function () {
     return view('admin.admin');
 });
-Route::get('/loginAdmin', function () {
-    return view('admin.loginAdmin');
+Route::get('/employeeLogin', function () {
+    return view('admin.employeeLogin');
 });
 /*         ADMIN       */
 
