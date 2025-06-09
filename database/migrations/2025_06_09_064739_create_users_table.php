1211074af946d2d93be3c9ad8ea3cbda746f3c24
Excorcist <gcodex999@gmail.com>
2025-06-09 14:10:25 +0700

    adding table migration

---
+++ database/migrations/2025_06_09_064739_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id'); // PK
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->text('alamat')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->text('pembelian')->nullable();
+            $table->text('pengembalian')->nullable();
+            $table->string('no_telepon');
+            $table->timestamps(); // Termasuk tanggal_dibuat
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
