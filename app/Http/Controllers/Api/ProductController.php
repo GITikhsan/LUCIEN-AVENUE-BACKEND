14874ea510345cd8daa69b6d1b5f9c443132edc3
Zaidan Elha <zaidanelhaa@gmail.com>
2025-06-16 16:08:54 +0700

    sambungin bagian home ke back end

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
     public function index()
     {
         // Menggunakan 'images' sesuai nama fungsi relasi di Model Product
-        $products = Product::with(['images', 'discount', 'promotion'])->latest()->paginate(10);
+        $products = Product::with(['images'])->latest()->paginate(10);
         return response()->json(['status' => true, 'data' => $products], 200);
     }
 
