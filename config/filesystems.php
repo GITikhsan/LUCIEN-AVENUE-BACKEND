651e50119d3ed2762e35061fb9a9b2d966375eaa
REXSY06 <bagusputrawiratama24@gmail.com>
2025-06-18 16:53:05 +0700

    pusing

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,11 +40,10 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('app/public'), // <-- INI SATU-SATUNYA BARIS YANG BERUBAH
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
-            'report' => false,
         ],
 
         's3' => [
